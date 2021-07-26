4d4adf67869cdc88958eec2f7f4e7c72f4b372cd
Nazarko19 <suhorukyinazar@gmail.com>
2021-07-26 13:28:22 +0300

    localstorage

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 
+
+// - Створити форму з інпутом для введення ім'я, та action="index2.html".
+// При відправці данних з форми, зберігати ім'я в localstorage, та переходити на index2.html.
+// На index2.html виводити записане в localstorage ім'я в div
+
+
+
 let form = document.forms.form
 let textiner = form.text
 let ok = form.submit
